dee1ad0162bcfcf5b0d43d49ed99f439d43be8ef
ARTHUR MEDEIROS GRIGOLI <arthurgrigoli6131@digitalcollege.internal>
2026-01-31 09:42:31 -0300

    feat: create an object to send in response

--- src/server.js
+++ src/server.js
@@ -1,7 +1,22 @@
 import http from 'node:http';
 
+
+const user = {
+    id: crypto.randomUUID(),
+    name: 'Fulanilson de tal',
+    phone: '859999999999',
+    adress: {
+        street: 'Rua do fulanilson',
+        number: 71
+    }
+};
+
+const userJSON = JSON.stringify(user);
+
+
+
 http.createServer((request, response)=>{
-    response.writeHead(200, {'content-type': 'text/html; charset=utf-8'})
-    response.end('Olá senhor!')
+    response.writeHead(200, {'content-type': 'application/json; charset=utf-8'})
+    response.end(userJSON)
 }).listen(3000);
 
